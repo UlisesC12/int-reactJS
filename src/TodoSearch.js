1e1505d019ec4408fe36111022a178b146ce111b
UlisesC12 <ulisescandela99@gmail.com>
2022-09-30 13:39:30 -0500

    state management in react

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,15 +2,20 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState("");
+
   const onSearchValueChange = (event) => {
     console.log(event.target.value);
+    setSearchValue(event.target.value);
   }
-  return (
+  return [ 
     <input 
       className='TodoSearch' 
       placeholder="Cebolla"
-      onChange={onSearchValueChange}/>
-  );
+      value={searchValue}
+      onChange={onSearchValueChange}/>,
+    <p>{searchValue}</p>
+  ];
 }
 
 export {TodoSearch};
