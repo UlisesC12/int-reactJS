65819bdac8a31ef4d1076b1fe20b47b2e66d94f7
UlisesC12 <ulisescandela99@gmail.com>
2022-10-17 18:42:41 -0500

    new commit from WSL

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import { TodoList } from "./TodoList";
 import { TodoItem } from "./TodoItem";
 import { TodoButton } from "./TodoButton";
 // import './App.css';
-const todos = [
+const defaultTodos = [
   { text: 'Cortar cebolla', completed: true },
   { text: 'Tomar el cursso de intro a React', completed: false },
   { text: 'Llorar con la llorona', completed: false },
   { text: 'LALALALAA', completed: false },
 ];
 function App(props) {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState("");
+
   return (
     <React.Fragment>
       <TodoCounter />
-      <TodoSearch />
-
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}/>
       <TodoList>
         {todos.map(todo => (
           <TodoItem
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch() {
-  const [searchValue, setSearchValue] = React.useState("");
+function TodoSearch({searchValue, setSearchValue}) {
 
   const onSearchValueChange = (event) => {
     console.log(event.target.value);
     setSearchValue(event.target.value);
   }
-  return [ 
+  return (
     <input 
       className='TodoSearch' 
       placeholder="Cebolla"
       value={searchValue}
-      onChange={onSearchValueChange}/>,
-    <p>{searchValue}</p>
-  ];
+      onChange={onSearchValueChange}/>
+  );
 }
 
 export {TodoSearch};
