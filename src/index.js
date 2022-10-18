d9200d299608545f15eac540ad782b43f97627ee
UlisesC12 <ulisescandela99@gmail.com>
2022-10-17 22:48:55 -0500

    Sort files and directories

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App/index.js';
 // import reportWebVitals from './reportWebVitals';
 import './index.css';
 
